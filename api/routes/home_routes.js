8a7493d7ca6fe221bf0f6ef4fd18fccbc94788ce
Arjun Raja <arjunraja@cybrilla.com>
2020-08-16 21:51:35 +0530

    changes api response and added more atrributes to projects model

--- api/routes/home_routes.js
+++ api/routes/home_routes.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const homeModel = require('../models/home')
-const app = require('../../app')
-const { route } = require('../../app')
-const { update, updateOne, remove } = require('../models/home')
+const mongoose = require('mongoose');
+const express = require('express');
+const homeModel = require('../models/home');
+const app = require('../../app');
+const { route } = require('../../app');
+const { update, updateOne, remove } = require('../models/home');
 
-const router = express.Router()
+const router = express.Router();
 
 /* Get Request
 - From homemodel find heading, desc
@@ -14,22 +14,20 @@ const router = express.Router()
 - catch the error
 */
 
-router.get('/',  async ( req, res, next ) => {
-    try
-    {
-        const homeResponse = await homeModel.find({}).select('heading desc').exec();
-        console.log('homeResponse: ', homeResponse);
-        return res.status(200).json({
-           count:  homeResponse.length,
-           projects: homeResponse
-        });
-    }
-    catch (err){
-        console.log(err);
-        return res.status(500).json({
-            error: err
-        });
-    }
+router.get('/', async (req, res, next) => {
+  try {
+    const homeResponse = await homeModel.find({}).select('heading desc').exec();
+    console.log('homeResponse: ', homeResponse);
+    return res.status(200).json({
+      count: homeResponse.length,
+      home: homeResponse,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+    });
+  }
 });
 
 /* Post request
@@ -40,49 +38,50 @@ router.get('/',  async ( req, res, next ) => {
 */
 
 router.post('/', async (req, res, next) => {
-    try {
-        console.log('req.body: ', req.body);
-        const home = homeModel({
-            _id: new mongoose.Types.ObjectId(),
-            heading: req.body.heading,
-            desc: req.body.desc
-        });
-        const homeResponse = await home.save();
-        return res.status(200).json({
-            message: "handling home create request",
-            home: homeResponse
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            error:err
-        });
-    }
+  try {
+    console.log('req.body: ', req.body);
+    const home = homeModel({
+      _id: new mongoose.Types.ObjectId(),
+      heading: req.body.heading,
+      desc: req.body.desc,
+    });
+    const homeResponse = await home.save();
+    return res.status(200).json({
+      message: 'handling home create request',
+      home: homeResponse,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: err,
+    });
+  }
 });
-
 
 /* Patch
 - Using loop to update the values
 - this will be an array of object
 - ex: [ {"propName":"heading", "value": "new heading"}]
 */
-router.patch('/:homeId', async(req,res,next) => {
-    try {
-        console.log('req.body: ', req.body);
-        const id = req.params.homeId;
-        const updateOps = {}
-        for (const ops of req.body) {
-            updateOps[ops.propName] = ops.value;
-        }
-        const result = await homeModel.update({_id: id}, {$set: updateOps}).exec();
-        console.log(result);
-        return res.status(200).json(result);
-    } catch (error) {
-        console.log(err);
-        return res.status(500).json({
-            error: err
-        });
+router.patch('/:homeId', async (req, res, next) => {
+  try {
+    console.log('req.body: ', req.body);
+    const id = req.params.homeId;
+    const updateOps = {};
+    for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
     }
+    const result = await homeModel
+      .update({ _id: id }, { $set: updateOps })
+      .exec();
+    console.log(result);
+    return res.status(200).json(result);
+  } catch (error) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+    });
+  }
 });
 
 /*
@@ -93,19 +92,18 @@ Delete
 - else handling the error using catch
 */
 
-router.delete('/:homeId', async(req, res, next) => {
-    try {
-        console.log('req.body: ', req.body);
-        const id = req.params.homeId;
-        const result = await homeModel.remove({_id: id}).exec();
-        return res.status(200).json(result)
-    } catch (error) {
-        console.log(err);
-        return res.status(500).json({
-            error: err
-        });
-    }
-
+router.delete('/:homeId', async (req, res, next) => {
+  try {
+    console.log('req.body: ', req.body);
+    const id = req.params.homeId;
+    const result = await homeModel.remove({ _id: id }).exec();
+    return res.status(200).json(result);
+  } catch (error) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+    });
+  }
 });
 
 module.exports = router;
--- api/routes/skill_routes.js
+++ api/routes/skill_routes.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const skillModel = require('../models/skill')
-const app = require('../../app')
-const { route } = require('../../app')
-const { update } = require('../models/home')
+const mongoose = require('mongoose');
+const express = require('express');
+const skillModel = require('../models/skill');
+const app = require('../../app');
+const { route } = require('../../app');
+const { update } = require('../models/home');
 
-const router = express.Router()
-
+const router = express.Router();
 
 /* Get Request
 - From skillModel find heading, desc
@@ -15,24 +14,24 @@ const router = express.Router()
 - catch the error
 */
 
-router.get('/',  async ( req, res, next ) => {
-    try
-    {
-        const skillResponse = await skillModel.find({}).select('language skill_name desc stars').exec();
-        console.log('skillResponse: ', skillResponse);
-        return res.status(200).json({
-           count:  skillResponse.length,
-           projects: skillResponse
-        });
-    }
-    catch (err){
-        console.log(err);
-        return res.status(500).json({
-            error: err
-        });
-    }
+router.get('/', async (req, res, next) => {
+  try {
+    const skillResponse = await skillModel
+      .find({})
+      .select('language skill_name desc stars')
+      .exec();
+    console.log('skillResponse: ', skillResponse);
+    return res.status(200).json({
+      count: skillResponse.length,
+      skills: skillResponse,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+    });
+  }
 });
-
 
 /* Post request
 - create skillModel
@@ -42,26 +41,26 @@ router.get('/',  async ( req, res, next ) => {
 */
 
 router.post('/', async (req, res, next) => {
-    try {
-        console.log('req.body: ', req.body);
-        const skill = skillModel({
-            _id: new mongoose.Types.ObjectId(),
-            language: req.body.language,
-            skill_name: req.body.skill_name,
-            desc : req.body.desc,
-            stars: req.body.skill
-        });
-        const skillResponse = await skill.save();
-        return res.status(200).json({
-            message: "handling skill create request",
-            home: skillResponse
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            error:err
-        });
-    }
+  try {
+    console.log('req.body: ', req.body);
+    const skill = skillModel({
+      _id: new mongoose.Types.ObjectId(),
+      language: req.body.language,
+      skill_name: req.body.skill_name,
+      desc: req.body.desc,
+      stars: req.body.skill,
+    });
+    const skillResponse = await skill.save();
+    return res.status(200).json({
+      message: 'handling skill create request',
+      home: skillResponse,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: err,
+    });
+  }
 });
 
 /* Patch
@@ -70,23 +69,25 @@ router.post('/', async (req, res, next) => {
 - ex: [ {"propName":"heading", "value": "new heading"}]
 */
 
-router.patch('/:skillId', async(req,res,next) => {
-    try {
-        console.log('req.body: ', req.body);
-        const id = req.params.skillId;
-        const updateOps = {}
-        for (const ops of req.body) {
-            updateOps[ops.propName] = ops.value;
-        }
-        const result = await skillModel.update({_id: id}, {$set: updateOps}).exec();
-        console.log(result);
-        return res.status(200).json(result);
-    } catch (error) {
-        console.log(err);
-        return res.status(500).json({
-            error: err
-        });
+router.patch('/:skillId', async (req, res, next) => {
+  try {
+    console.log('req.body: ', req.body);
+    const id = req.params.skillId;
+    const updateOps = {};
+    for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
     }
+    const result = await skillModel
+      .update({ _id: id }, { $set: updateOps })
+      .exec();
+    console.log(result);
+    return res.status(200).json(result);
+  } catch (error) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+    });
+  }
 });
 
 /*
@@ -97,19 +98,18 @@ Delete
 - else handling the error using catch
 */
 
-router.delete('/:skillId', async(req, res, next) => {
-    try {
-        console.log('req.body: ', req.body);
-        const id = req.params.skillId;
-        const result = await skillModel.remove({_id: id}).exec();
-        return res.status(200).json(result)
-    } catch (error) {
-        console.log(err);
-        return res.status(500).json({
-            error: err
-        });
-    }
-
+router.delete('/:skillId', async (req, res, next) => {
+  try {
+    console.log('req.body: ', req.body);
+    const id = req.params.skillId;
+    const result = await skillModel.remove({ _id: id }).exec();
+    return res.status(200).json(result);
+  } catch (error) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+    });
+  }
 });
 
 module.exports = router;
