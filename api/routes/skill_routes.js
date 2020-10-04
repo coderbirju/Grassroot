6608e33b8d9f62db563552d781b37cdc9f51a85d
Shrikrishna Joisa <shrikrishnajois@gmail.com>
2020-10-04 18:27:16 +0530

    Updated the card_style api

--- api/routes/skill_routes.js
+++ api/routes/skill_routes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res, next) => {
   try {
     const skillResponse = await skillModel
       .find({})
-      .select('logo skill_name desc link')
+      .select('logo skill_name desc link card_style')
       .exec();
     console.log('skillResponse: ', skillResponse);
     return res.status(200).json({
